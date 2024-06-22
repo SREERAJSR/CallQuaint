280f398f48a9ee6ed22080a634c08fdd4ca64aa1
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2024-06-22 23:43:45 +0530

    compiled firsttime on production

--- client/src/app/routeguards/canActivate/auth.guard.ts
+++ client/src/app/routeguards/canActivate/auth.guard.ts
@@ -1,13 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService: AuthService = inject(AuthService)
+  const router :Router = inject(Router)
   const accessToken = authService.getAccessToken();
   const refreshToken = authService.getRefreshToken()
-  if (!accessToken && !refreshToken)
+  if (!accessToken && !refreshToken) {
     return true;
+  }
+
+  router.navigate(['/'])
   return false;
 
 };
